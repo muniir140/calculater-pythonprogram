d3de74f0c488f4108a594736803433832af1aec3
Muniir <183903429+muniir140@users.noreply.github.com>
2025-07-03 10:10:28 +0300

    Update calc-program.py

--- calc-program.py
+++ calc-program.py
@@ -1,24 +1,25 @@
-Operater = input("Enter the operater (+ - * / **)")
+Operater = input("Enter the operator (+ - * / **): ")
 
-num1 = float(input("Enter the 1st number:"))
-num2 = float(input("Enter the 2nd number:"))
+num1 = float(input("Enter the 1st number: "))
+num2 = float(input("Enter the 2nd number: "))
 
-# print(num1 + num2)
-# print(round(result,3))
 if Operater == "+":
-  result = num1 +num2
-  print(round(result,3))
-elif Operater =="-":
- result = num1-num2
- print(round(result,3))
-elif Operater =="*":
-  result = num1 *num2
-  print(round(result,3))
-elif Operater =="/":
-  result = num1 /num2
-  print(round(result,3))
-elif Operater =="**":
-  result = num1 /num2
-  print(round(result,3))
+    result = num1 + num2
+    print(round(result, 3))
+elif Operater == "-":
+    result = num1 - num2
+    print(round(result, 3))
+elif Operater == "*":
+    result = num1 * num2
+    print(round(result, 3))
+elif Operater == "/":
+    if num2 == 0:
+        print("Cannot divide by zero!")
+    else:
+        result = num1 / num2
+        print(round(result, 3))
+elif Operater == "**":
+    result = num1 ** num2
+    print(round(result, 3))
 else:
-  print(f"{Operater} is not a valid  operator")
+    print(f"{Operater} is not a valid operator.")
